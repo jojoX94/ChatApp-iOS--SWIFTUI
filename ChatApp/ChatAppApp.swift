e88734c676d02d7b748f026640132de842ade3c2
Tahina R <ramarolacontact@gmail.com>
2022-10-17 17:38:31 +0300

    Init Project's Structure

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Madiapps on 17/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
