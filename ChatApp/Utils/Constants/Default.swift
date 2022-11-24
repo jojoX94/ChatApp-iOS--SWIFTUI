1207719587ec55c89a2b04092d4102e933fefd03
jojoX94 <ramarolacontact@gmail.com>
2022-11-24 15:21:48 +0300

    Modify auth workflow to dynamic

---
+++ ChatApp/Utils/Constants/Default.swift
@@ -0,0 +1,17 @@
+//
+//  Default.swift
+//  ChatApp
+//
+//  Created by Madiapps on 24/11/2022.
+//
+
+import Foundation
+
+struct Defaults {
+    static let pushUserId = "PushUserId"
+    
+    static let accessToken = "AccessToken"
+    static let refreshToken = "RefreshToken"
+    
+    static let userName = "UserName"
+}
